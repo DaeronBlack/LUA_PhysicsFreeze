209aa5ee596b9b433dcc9092ee57ae513bf130d5
BuildTools <unconfigured@null.spigotmc.org>
2018-06-14 21:20:16 +0200

    Version2.0

--- main.lua
+++ main.lua
@@ -5,13 +5,13 @@
 -- Time: 20:07
 -- To change this template use File | Settings | File Templates.
 --
---[[
+
 function GODMODE( ply, dmginfo) if(ply:GetNWInt("GOD") == 1) then
-    dmginfo:ScaleDamage(0)
+        dmginfo:ScaleDamage(0)
     end
-end
 
 hook.Add("EntityTakeDamage", "GODMODE", GODMODE)
+    end
 
 hook.add("PhysgunDrop", "ply_physgunfreeze", function(pl, ent)
     hook.Remove("PhysgunDrop", "ulxPlayerDrop")
@@ -21,6 +21,7 @@ hook.add("PhysgunDrop", "ply_physgunfreeze", function(pl, ent)
 
     if(pl:KeyDown(IN_ATTACK2)) then
         ent:Freeze(true)
+        RunCOnsoleCommand( "use", "keys" )
         ent:SetNWInt(GODMODE, 1)
         ent:DisallowSpawning( not should_unfreeze)
         ulx.setNoDie(ent, not should_unfreeze)
@@ -41,26 +42,20 @@ hook.add("PhysgunDrop", "ply_physgunfreeze", function(pl, ent)
 end
 end)
 
-    hook.Add("PhysgunPickup", "ply_frozen", function(pl, ent)
+hook.Add("PhysgunPickup", "ply_frozen", function(pl, ent)
+    if (ent:IsPlayer() )then
         ent._frozen = true
-    end)
+        RunConsoleCommand( "use", "keys")
+    else
+        return false
+    end
+end)
 
     function playerdies( pl, weapon, killer ) if(pl._frozen)then
         return false
     else
-    return true
+        return true
     end
 
-    hook.Add("CanPlayerSuicide", "playerNoDeath", playerDies)
+hook.Add("CanPlayerSuicide", "playerNoDeath", playerDies)
     end
-]]--
-function PlayerPickup( pl, ent )
-    if ( ent:IsPlayer() )then
-        print( "Entity is a player!" )
-        RunConsoleCommand( "use","keys" )
-    else
-        print( "Entity is not a player!" )
-        return false
-    end
-end
-hook.Add( "PhysgunPickup", "Switch to keys", PlayerPickup )
